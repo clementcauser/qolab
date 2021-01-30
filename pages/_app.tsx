0e8219c5dbdc22b142cee3b482b1c3f8f5a0b13c
Clément Causer <ccauser@simplonprod.co>
2021-01-30 21:07:03 +0100

    💄  Add Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import "../styles/globals.css";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import { useEffect } from "react";
+
+import "../styles/globals.css";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
