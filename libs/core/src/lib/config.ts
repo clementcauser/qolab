1ff178a35c20bd3cf5d88ac9e875ffb21176c7c0
Clément Causer <ccauser@simplonprod.co>
2022-05-15 12:56:19 +0200

    🐛 Fix PORT env variable

--- libs/core/src/lib/config.ts
+++ libs/core/src/lib/config.ts
@@ -1,4 +1,4 @@
 export const configuration = () => ({
   environment: process.env.NODE_ENV,
-  port: parseInt(process.env.API_PORT || '3333', 10),
+  port: parseInt(process.env.PORT || '3333', 10),
 });
