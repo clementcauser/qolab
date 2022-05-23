31da21d6b8c7e0326d5b0a5c2b4e6b2553bbb55e
Clément Causer <ccauser@simplonprod.co>
2022-05-23 10:19:13 +0200

    ➖ remove uikit-e2e lib

--- apps/uikit-e2e/src/integration/uikit/uikit.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('uikit: Uikit component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=uikit--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to Uikit!');
-    });
-});
