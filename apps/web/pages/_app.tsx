900bdd537db15d4448a0e794e1249c4cc47ff843
Clément Causer <ccauser@simplonprod.co>
2022-05-23 10:39:08 +0200

    🍱 Add favicon

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,7 @@
+import { theme } from '@qolab/uikit';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 
 import './styles.css';
 
@@ -8,9 +10,29 @@ function CustomApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>qolab - Développez vos applications solidaires</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className="app">
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </main>
     </>
   );
