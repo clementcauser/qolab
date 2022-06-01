94288beb922e6031a80d7b7ead9e96fdab64557d
Clément Causer <ccauser@simplonprod.co>
2022-06-01 21:12:28 +0200

    ⚰️ Remove temporary code

--- libs/core/src/lib/core.resolver.ts
+++ libs/core/src/lib/core.resolver.ts
@@ -1,16 +1,9 @@
-import { UseGuards } from '@nestjs/common';
 import { Float, Query, Resolver } from '@nestjs/graphql';
-import { CurrentUser, GqlAuthGuard, JwtToken } from '@qolab/auth';
 
 @Resolver()
 export class CoreResolver {
   @Query(() => Float)
-  @UseGuards(GqlAuthGuard)
-  uptime(@CurrentUser() user: JwtToken) {
-    console.log(
-      '🚀 ~ file: core.resolver.ts ~ line 10 ~ CoreResolver ~ uptime ~ user',
-      user
-    );
+  uptime() {
     return process.uptime();
   }
 }
